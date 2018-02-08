109cdf11153ad92474f8640dc70b97fda14cff7a
cwooley-univec <cwooley@univecinc.com>
2018-02-08 16:06:27 -0500

    switched spaceship by support to a picture of myself

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -17,7 +17,7 @@ export default class TopMenu extends Component{
           position={'right'}
         >
           <a style={{paddingRight:10, fontSize:20}}>Support</a>
-          <Image style={{marginRight:10}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-yiXNE7kEemvWApuphU4LxUNToU6XsM-8accZ2s15uMs3yyH4mg' avatar />
+          <Image style={{marginRight:10, borderRadius:2}} src='https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/19667823_10155576185331579_5655974443043484940_o.jpg?oh=92af44f5b5452fe90960ff036b25e430&oe=5B232D79' avatar />
         </Menu.Item>
       </Menu>
     )
